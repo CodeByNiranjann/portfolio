401d62a9b3878c11d4e38bd9f4dab204b1f7dc8b
Niranjan <amuthanabishekniranjan@gmail.com>
2026-06-19 00:29:41 +0530

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,10 @@ export default function About() {
         </diV>
         <div className='w-1/2 flex justify-center'>
         <div className='flex flex-col justify-center'>
-             <h1 className='text-5xl '>About Me</h1>
-            <p>Hi, I am Niranjan, a software developer and designer. I have a passion for creating innovative solutions and designing user-friendly interfaces. 
-                With experience in various programming languages and design tools, I strive to deliver high-quality work that meets the needs of clients and users alike.</p>
+             <h1 className='text-5xl text-white  mb-5 font-bold '>About Me</h1>
+            <p className='text-black font-bold pb-6'><i>Hi, I am Niranjan, a software developer and designer. I have a passion for creating innovative solutions and designing user-friendly interfaces.</i> </p>
+            <p className='text-black font-bold pb-6'><i>With experience in various programming languages and design tools, I strive to deliver high-quality work that meets the needs of clients and users alike.</i></p>
+            <p className='text-black font-bold pb-6'><i>Im a <span className='text-white font-bold'>MERNSTACK</span> Developer.</i></p>
 
         </div>
            
---
+++ src/components/Projects.js
@@ -0,0 +1,18 @@
+export default function Projects() {
+    return <section className="flex px-5 py-5 bg-secondary justify-center">
+        <div className="w-1/2">
+        <di v className="flex  justify-center">
+             <h1>Heading </h1>
+
+        </di>
+         </div >
+        <h1 className="w-1/2">Projects</h1>
+
+
+        <div>
+
+        </div>
+
+    </section>
+
+}
