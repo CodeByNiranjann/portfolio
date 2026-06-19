ad9b8a8d331bf75fa4298a08dbe28e290caa4ace
Niranjan <amuthanabishekniranjan@gmail.com>
2026-06-19 23:26:28 +0530

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ export default function About() {
         <div className='w-1/2 flex justify-center'>
         <div className='flex flex-col justify-center'>
              <h1 className='text-5xl text-white  mb-5 font-bold '>About Me</h1>
-            <p className='text-black font-bold pb-6'><i>Hi, I am Niranjan, a software developer and designer. I have a passion for creating innovative solutions and designing user-friendly interfaces.</i> </p>
+            <p className='text-black font-bold pb-6'><i>Hi, I'm Niranjan, a MERN Stack Developer with a strong passion for building scalable web applications and creating intuitive, user-centered digital experiences.</i> </p>
             <p className='text-black font-bold pb-6'><i>With experience in various programming languages and design tools, I strive to deliver high-quality work that meets the needs of clients and users alike.</i></p>
-            <p className='text-black font-bold pb-6'><i>Im a <span className='text-white font-bold'>MERNSTACK</span> Developer.</i></p>
+            <p className='text-black font-bold pb-6'><i>I am continuously improving my development skills through projects, self-learning, and hands-on practice. My goal is to gain real-world experience, work on exciting challenges, and grow as a software developer.</i></p>
 
         </div>
            
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,18 @@
+import about from '../assets/about.png'
 export default function Projects() {
     return <section className="flex px-5 py-5 bg-secondary justify-center">
         <div className="w-1/2">
-        <di v className="flex  justify-center">
-             <h1>Heading </h1>
+        <di  className="flex  justify-center ">
+             <h1 className="text-4xl text-white  mb-5 font-bold ">PROJECTS </h1>
+             
 
         </di>
          </div >
-        <h1 className="w-1/2">Projects</h1>
+        <h1 className="w-1/2"></h1>
+        <div>
+            <img src={about}/>
+        </div>
+        
 
 
         <div>
