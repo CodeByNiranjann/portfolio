6f4b86d074c0dec109c5a0eee521cc033ac85624
Niranjan <amuthanabishekniranjan@gmail.com>
2026-06-21 21:03:02 +0530

    commites

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import about from '../assets/about.png'
+import hero from '../assets/HeroImg.png'
 export default function Projects() {
-    return <section className="flex px-5 py-5 bg-secondary justify-center">
+    return <section className="flex flex-col px-5 py-5 bg-secondary justify-center">
         <div className="w-1/2">
         <di  className="flex  justify-center ">
              <h1 className="text-4xl text-white  mb-5 font-bold ">PROJECTS </h1>
@@ -8,16 +9,30 @@ export default function Projects() {
 
         </di>
          </div >
-        <h1 className="w-1/2"></h1>
-        <div>
-            <img src={about}/>
+        <div className='w-1/2'>
+            <div className='flex px-10 gap-1'>
+                <div>
+                    <img className='h-[300px]' src={hero}></img>
+                    <div>
+                        <p>l</p>
+                    </div>
+                </div>
+                <div>
+                    <img className='h-[200px]' src={hero}></img>
+                </div>
+               <div>
+                 <img className='h-[200px]' src={hero}></img>
+               </div>
+
+            </div>
+
         </div>
+
+
+         
+
+
         
-
-
-        <div>
-
-        </div>
 
     </section>
 
